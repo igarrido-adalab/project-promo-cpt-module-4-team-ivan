1e6707cc3d3795e50f5fbebb32714ce61409447b
Iván (profe) <ivan@adalab.es>
2025-03-22 12:25:50 +0100

    Add port env variable.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json({limit: '25Mb'}));
 app.set('view engine', 'ejs');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Cool Projects server started at <http://localhost:${PORT}/>`);  
 });
